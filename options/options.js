624d82d5d933313304d1f48fdc9beb563ed8db04
Crowdbotics <team@crowdbotics.com>
2023-07-07 11:49:07 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "royal-cherry-41968",
+  url: "https://royal-cherry-41968.botics.co",
+  api: "https://royal-cherry-41968.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
